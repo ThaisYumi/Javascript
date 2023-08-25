d7a6d59df518bfce7b48d20f487a711b5c1fe452
thaisyumi <yumihorikawa@hotmail.com>
2023-08-24 21:53:52 -0300

    Update js.js

--- exercicios/ex005/js.js
+++ exercicios/ex005/js.js
@@ -1,11 +1,12 @@
-var num = document.getElementById(`num`)
-var display = document.getElementById(`display`)
-var vetor = []
+let num = document.getElementById(`num`)
+let display = document.getElementById(`display`)
+let vetor = []
 let res = document.getElementById(`res`)
 
 function adicionar() {
     let n = Number(num.value)
     res.innerText = ``
+    num.focus()
 
     if (num.value.length == 0) {
         window.alert(`[ERRO]: Adicione um numero!`)
@@ -13,14 +14,18 @@ function adicionar() {
     } else if (num.value < 1 || num.value > 100) {
         window.alert(`[ERRO]: Numero precisa ser entre  1 e 100!`)
         num.value = ``
+        num.focus()
 
     } else if (vetor.indexOf(n) > -1) {
             window.alert(`[ERRO]: Numero ja encontrado na lista`)
             num.value = ``
+            num.focus()
 
     } else {
             vetor.push(n)
             num.value = ``
+            num.focus()
+            vetor.sort((a, b) => a - b);
             
             let item = document.createElement(`option`)
             item.text = `Valor ${n} adicionado!`
@@ -30,26 +35,24 @@ function adicionar() {
 }
 
 function finalizar() {
-    
-    res.innerText = ``
-    
-    if (vetor.length == 0) {
-        window.alert(`[ERRO]: Adicione um numero!`)
+        res.innerText = ``
+        num.focus()
+        
+        if (vetor.length == 0) {
+            window.alert(`[ERRO]: Adicione um numero!`)
 
-    } else {
-    vetor.sort()
-    let s = 0
+        } else {
+        let s = 0
 
-    for(let c = 0; c < vetor.length; c++) {
-        s += Number(vetor[c])
+        for(let c = 0; c < vetor.length; c++) {
+            s += Number(vetor[c])
+        }
+        
+        let m = s / vetor.length
+        res.innerHTML += `Ao todo temos ${vetor.length} numeros cadastrados. <br>`
+        res.innerHTML += `O maior valor informado foi ${vetor[vetor.length - 1]}. <br>`
+        res.innerHTML += `O menor valor informado foi ${vetor[0]}. <br>`
+        res.innerHTML += `Somando todos os valores, temos ${s}. <br>`
+        res.innerHTML += `A media dos valores digitados e ${m}.`
     }
-
-    let m = s / vetor.length
-
-    res.innerHTML += `Ao todo temos ${vetor.length} numeros cadastrados. <br>`
-    res.innerHTML += `O maior valor informado foi ${vetor[vetor.length-1]}. <br>`
-    res.innerHTML += `O menor valor informado foi ${vetor[0]}. <br>`
-    res.innerHTML += `Somando todos os valores, temos ${s}. <br>`
-    res.innerHTML += `A media dos valores digitados e ${m}.`
-}
 }
